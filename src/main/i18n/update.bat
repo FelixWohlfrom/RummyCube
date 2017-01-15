335bf7da38dbf40776cd43518cd8c9473649bd5c
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2017-01-15 14:12:00 +0100

    added some help in localisation update scripts

--- src/main/i18n/update.bat
+++ src/main/i18n/update.bat
@@ -1,5 +1,13 @@
 @echo off
 
+if "%1" == "-h" (
+    echo Usage: update.bat [-r]
+	echo Will update the localisation strings from source files.
+	echo Paramters:
+	echo  -r    Will remove obselete resources
+	exit /B 0
+)
+
 set remove_unused=
 if "%1" == "-r" (
     set remove_unused=-no-obsolete
--- src/main/i18n/update.sh
+++ src/main/i18n/update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ "x$1" = "x-h" ]]; then
+    echo "Usage: update.sh [-r]"
+	echo "Will update the localisation strings from source files."
+	echo "Paramters:"
+	echo " -r    Will remove obselete resources"
+	exit 0
+fi
+
 remove_unused=""
 if [[ "x$1" = "x-r" ]]; then
     remove_unused="-no-obsolete"
